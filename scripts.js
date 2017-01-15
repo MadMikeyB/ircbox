23818d3e79b724ebed6b2400cb65f88c21266e80
Michael <mikeybsaccount@gmail.com>
2017-01-15 02:26:30 +0000

    merge in unic0rn changes

--- scripts.js
+++ scripts.js
@@ -20,8 +20,8 @@ $(function() {
         console.log('onopen called');
         console.log(event);
         $('#output').append('<p><strong style="color: #00CD00">CONNECTED</strong></p>');
-        websocket.send('USER iamdevloper * * :WebSocket User');
-        websocket.send('NICK [fn]_mikey'); // @todo var nick
+        websocket.send('USER iamdevloper * * :WebSocket User\n');
+        websocket.send('NICK [fn]_mikey\n'); // @todo var nick
     }
 
     function close(event) {
@@ -41,30 +41,49 @@ $(function() {
         console.log('onmessage called');
         console.log(event);
         rawData = event.data;
-       if (rawData instanceof Blob)
-       {
-           var fileReader = new FileReader();
-           fileReader.addEventListener("loadend",handleBinaryInput);
-           fileReader.readAsText(rawData);
-       }
-       else
-       {
-            if (rawData.indexOf('PING') == 0)
-            {
-                pongResponse = rawData.replace('PING','PONG');
-                console.log('sending PONG');
-                websocket.send(pongResponse);
-            }
-            else if (rawData.indexOf('376') > -1)
-            {
-                websocket.send('JOIN #tiramisu'); // @todo var chan
-            }
-            else if ( rawData.indexOf('422') > -1 ) {
-                websocket.send('JOIN #tiramisu');
-            }
-            else if ( rawData.indexOf('433') > -1 ) {
-                websocket.send('NICK [fn]_mikey_'+Math.floor((Math.random() * 1000) + 1).toString() );
-            }
+        if (rawData instanceof Blob)
+        {
+            var fileReader = new FileReader();
+            fileReader.addEventListener("loadend",handleBinaryInput);
+            fileReader.readAsText(rawData);
         }
+        else
+        {
+            process(rawData);
+        }
+    }
+
+    function process(message) {
+        console.log(message);
+        msgsplit = message.split(":");
+        msgsplit2 = msgsplit[1].split(" ");
+        cmd = msgsplit2[1];
+        target = msgsplit2[2];
+        srcnick = msgsplit2[0].split("!")[0];
+        rest = message.slice(message.indexOf(':',1)+1,-1);
+        if (message.indexOf('PING') == 0)
+        {
+            pongResponse = message.replace('PING','PONG');
+            console.log('sending PONG');
+            websocket.send(pongResponse);
+        }
+        else if ((cmd == '376') || (cmd == '422'))
+        {
+            websocket.send('JOIN #tiramisu\n'); // @todo var chan
+        }
+        else if ( cmd == '433' )
+        {
+            websocket.send('NICK [fn]_mikey_'+Math.floor((Math.random() * 1000) + 1).toString()+'\n' );
+        }
+        else if ( (cmd == 'PRIVMSG') && (target == '#tiramisu') )
+        {
+            document.getElementById('output').innerHTML += srcnick+': '+rest+'<br />';
+        }
+    }
+
+    function handleBinaryInput(event) {
+        var fileReader = event.target;
+        var raw = fileReader.result;
+        process(raw);
     }
 });
