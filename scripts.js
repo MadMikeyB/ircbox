d0436da0ee0af787064fd4436a51be7218c10f00
unic0rn <mirek.kaim@gmail.com>
2017-01-15 02:05:23 +0100

    another fix

--- scripts.js
+++ scripts.js
@@ -37,21 +37,20 @@ $(function() {
             }
             function process(message) {
               console.log(message);
+              msgsplit = message.split(":");
+              cmd = msgsplit[1].split(" ")[1];
               if (message.indexOf('PING') == 0)
               {
                   pongResponse = message.replace('PING','PONG');
                   console.log('sending PONG');
                   websocket.send(pongResponse);
               }
-              else if (message.indexOf('376') > -1)
+              else if ((cmd == '376') || (cmd == '422'))
               {
-                  websocket.send('JOIN #tiramisu'); // @todo var chan
+                  websocket.send('JOIN #tiramisu\n'); // @todo var chan
               }
-              else if ( message.indexOf('422') > -1 ) {
-                  websocket.send('JOIN #tiramisu');
-              }
-              else if ( message.indexOf('433') > -1 ) {
-                  websocket.send('NICK [fn]_mikey_'+Math.floor((Math.random() * 1000) + 1).toString() );
+              else if ( cmd == '433' ) {
+                  websocket.send('NICK [fn]_mikey_'+Math.floor((Math.random() * 1000) + 1).toString()+'\n' );
               }
             }
             websocket.onclose = function(event) {
