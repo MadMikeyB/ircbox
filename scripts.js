9cfe8c6a4a5731cfe5c72bde0f5a0592ad8833e6
Michael <mikeybsaccount@gmail.com>
2017-01-15 00:53:02 +0000

    try new things

--- scripts.js
+++ scripts.js
@@ -10,40 +10,61 @@ $(function() {
         websocket = new WebSocket('ws://aurora.irc.arloria.net:8081/');
         console.log('connection established');
 
-        websocket.onopen =  function(message) {
-            console.log('onopen called');
-            console.log(message);
-            websocket.send('USER iamdevloper * * :WebSocket User');
-            websocket.send('NICK [fn]_mikey'); // @todo var nick
-            websocket.onmessage = function(message) {
-                console.log('onmessage called');
-                console.log(message);
-                if (message.indexOf('PING') == 0)
-                {
-                    pongResponse = message.replace('PING','PONG');
-                    console.log('sending PONG');
-                    websocket.send(pongResponse);
-                }
-                else if (message.indexOf('376') > -1)
-                {
-                    websocket.send('JOIN #tiramisu'); // @todo var chan
-                }
-                else if ( message.indexOf('422') > -1 ) {
-                    websocket.send('JOIN #tiramisu');
-                }
-                else if ( message.indexOf('433') > -1 ) {
-                    websocket.send('NICK [fn]_mikey_'+Math.floor((Math.random() * 1000) + 1).toString() );
-                }
+        websocket.onopen =  function(event) { open(event); }
+        websocket.onmessage = function(event) { message(event); }
+        websocket.onclose = function(event) { close(event); }
+        websocket.onerror = function(event) { error(event); }
+    }
+
+    function open(event) {
+        console.log('onopen called');
+        console.log(event);
+        $('#output').append('<p><strong style="color: #00CD00">CONNECTED</strong></p>');
+        websocket.send('USER iamdevloper * * :WebSocket User');
+        websocket.send('NICK [fn]_mikey'); // @todo var nick
+    }
+
+    function close(event) {
+        console.log('onclose called');
+        $('#output').append('<p><strong style="color: #fca000">DISCONNECTED</strong></p>');
+        console.log('connection closed');
+    }
+
+    function error(event) {
+        console.log('onerror called');
+        $('#output').append('<p><strong style="color: #CD0000">ERROR!!!</strong></p>');
+
+        console.log(event); 
+    }
+
+    function message(event) {
+        console.log('onmessage called');
+        console.log(event);
+        rawData = event.data;
+       if (rawData instanceof Blob)
+       {
+           var fileReader = new FileReader();
+           fileReader.addEventListener("loadend",handleBinaryInput);
+           fileReader.readAsText(rawData);
+       }
+       else
+       {
+            if (rawData.indexOf('PING') == 0)
+            {
+                pongResponse = rawData.replace('PING','PONG');
+                console.log('sending PONG');
+                websocket.send(pongResponse);
             }
-            websocket.onclose = function(event) {
-                console.log('onclose called');
-                console.log('connection closed');
+            else if (rawData.indexOf('376') > -1)
+            {
+                websocket.send('JOIN #tiramisu'); // @todo var chan
             }
-            websocket.onerror = function(event) {
-                console.log('onerror called');
-                console.log(event.data);
+            else if ( rawData.indexOf('422') > -1 ) {
+                websocket.send('JOIN #tiramisu');
+            }
+            else if ( rawData.indexOf('433') > -1 ) {
+                websocket.send('NICK [fn]_mikey_'+Math.floor((Math.random() * 1000) + 1).toString() );
             }
         }
     }
-    
 });
